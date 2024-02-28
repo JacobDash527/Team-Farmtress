2cb202f1cac6a1086f6c62b7a447e85bc5865905
Jacob Dash <jacobdash527@gmail.com>
2024-02-28 21:08:14 +0800

    Added tp sprite Removed background of arrow

--- src/classes.py
+++ src/classes.py
@@ -26,8 +26,9 @@ class Button():
 class SpriteSheet():
     def __init__(self, image):
         self.sheet = image
-    def get_image(self, frame, width, height, scale):
-        image = pygame.Surface((width, height)).convert_alpha()
+    def get_image(self, frame, width, height, scale, colour):
+        image = pygame.Surface((width, height))
         image.blit(self.sheet, (0, 0), ((frame * width), 0, width, height))
         image = pygame.transform.scale(image, (width * scale, height * scale))
+        image.set_colorkey(colour)
         return(image)
--- src/main.py
+++ src/main.py
@@ -9,26 +9,26 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('FNAP')
-bread_monster = pygame.transform.scale(pygame.image.load("images/bread_monster.png"), (100, 100))
-bg = pygame.image.load("images/bg.png")
+bread_monster = pygame.transform.scale(pygame.image.load("images/teleporter.png"), (100, 100))
+bg = pygame.image.load("images/bg.png").convert_alpha()
 arrow_sheet = classes.SpriteSheet(pygame.image.load("sprite_sheets/arrow_sheet.png"))
 arrow_frame_list = []
 frame = 0
 
 clock = pygame.time.Clock()
-button = classes.Button(420, 60, bread_monster)
+button = classes.Button(400, 300, bread_monster)
 
 run = True
 
 while run:
   clock.tick(10)
 
+  screen.blit(bg, (0, 0))
+
   button.draw()
 
-  screen.blit(arrow_sheet.get_image(frame, 15, 23, 10), (0, 0))
-
   if button.colliding(pygame.mouse.get_pos()) == True:
-    print("whomp whomp")
+    screen.blit(arrow_sheet.get_image(frame, 15, 23, 4, (0, 0, 0)), (button.x + 20, button.y - 100))
 
   for event in pygame.event.get():
     if event == pygame.QUIT:
