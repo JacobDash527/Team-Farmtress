cb885acf586832deb1bd57178228f1c8fa8bc2bb
Jacob Dash <jacobdash527@gmail.com>
2024-04-14 18:58:50 +0800

    Stuff idk

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+#[x, y, button, plant, menu_open]
 plots = []
 plots.append([[1, 1, 'null', 'null', False], [2, 1, 'null', 'null', False], [3, 1, 'null', 'null', False], [4, 1, 'null', 'null', False], [5, 1, 'null', 'null', False]])
 plots.append([[1, 2, 'null', 'null', False], [2, 2, 'null', 'null', False], [3, 2, 'null', 'null', False], [4, 2, 'null', 'null', False], [5, 2, 'null', 'null', False]])
@@ -17,15 +18,18 @@ plots.append([[1, 3, 'null', 'null', False], [2, 3, 'null', 'null', False], [3, 
 plots.append([[1, 4, 'null', 'null', False], [2, 4, 'null', 'null', False], [3, 4, 'null', 'null', False], [4, 4, 'null', 'null', False], [5, 4, 'null', 'null', False]])
 plots.append([[1, 5, 'null', 'null', False], [2, 5, 'null', 'null', False], [3, 5, 'null', 'null', False], [4, 5, 'null', 'null', False], [5, 5, 'null', 'null', False]])
 
+inventory = {"money":0, "single_harvest_seeds":0, "multi_harvest":0, "seed_dropper":0}
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('teamfarmtress')
+pygame.display.set_caption('Team Farmtress')
 arrow_frame_list = []
 frame = 0
+text_font = pygame.font.SysFont("ROG Fonts Regular", 30)
 
-intro_vid = pyvidplayer.Video("Mr Pixtel.mp4")
-intro_vid.set_size((800, 450))
+# intro_vid = pyvidplayer.Video("Mr Pixtel.mp4")
+# intro_vid.set_size((800, 450))
 
-breads_menu = ui_classes.SideMenus(pygame.image.load("images/breads_button.png").convert_alpha(), pygame.image.load("images/breads_menu.png").convert_alpha())
+# breads_menu = ui_classes.SideMenus(pygame.image.load("images/breads_button.png").convert_alpha(), pygame.image.load("images/breads_menu.png").convert_alpha())
 
 clock = pygame.time.Clock()
 
@@ -35,8 +39,16 @@ for xindex, row in enumerate(plots):
   for yindex, column in enumerate(row):
     column[2] = ui_classes.Button(xindex * 90 + 80, yindex * 90 + 125, textures.teleporter, 0.70)
 
-def plot_menu(x, y):
-  pass
+def plant_menu(plot_x, plot_y):
+  screen.blit(textures.plant_menu, (plot_x, plot_y))
+  x, y = pygame.mouse.get_pos()
+  if pygame.mouse.get_pressed()[0] == 1:
+    if (plot_y + 27) < y < (plot_y + 40):
+      pass
+    elif (plot_y + 49) < y < (plot_y + 59):
+      pass
+    elif (plot_y + 71) < y < (plot_y + 84):
+      pass
 
 def intro():
   intro_playing = True
@@ -55,6 +67,9 @@ def intro():
         pygame.display.update()
         intro_playing = False
 
+def draw_text(text, font, text_col, x, y):
+  img = font.render(text, True, text_col)
+  screen.blit(img, (x, y))
 
 #intro()
 
@@ -63,15 +78,23 @@ while run:
 
   screen.blit(textures.bg, (0, 0))
 
-  breads_menu.draw()
+  screen.blit(textures.breads_menu, (548, 20))
 
   for xindex, row in enumerate(plots):
     for yindex, column in enumerate(row):
       column[2].draw(xindex * 90 + 60, yindex * 90 + 125)
-      if column[2].colliding(pygame.mouse.get_pos()) == True:
-        screen.blit(textures.arrow_sheet.get_image(frame, 15, 23, 4, (0, 0, 0)), (column[2].x + 8, column[2].y - 80))
+
+  for xindex, row in enumerate(plots):
+    for yindex, column in enumerate(row):
+      if column[2].colliding(pygame.mouse.get_pos()):
+        screen.blit(textures.arrow_sheet.get_image(frame, 15, 23, 4, (0, 0, 0)), (column[2].x - 10, column[2].y - 80))
         if column[2].is_clicked() == True:
-          column[4]
+          column[4] = True
+
+      if column[4]:
+        print(plant_menu(xindex * 90 + 60, yindex * 90 + 125))
+
+  draw_text("big black cock", text_font, (0, 0, 0), 100, 100)
 
   for event in pygame.event.get():
     if event == pygame.QUIT:
--- src/textures.py
+++ src/textures.py
@@ -5,3 +5,5 @@ bread_monster = pygame.transform.scale(pygame.image.load("images/bread_monster.p
 bg = pygame.image.load("images/bg.png").convert_alpha()
 arrow_sheet = ui_classes.SpriteSheet(pygame.image.load("sprite_sheets/arrow_sheet.png"))
 teleporter = pygame.transform.scale(pygame.image.load("images/teleporter.png"), (120, 100))
+plant_menu = pygame.image.load("images/plantmenu.png").convert_alpha()
+breads_menu = pygame.transform.scale(pygame.image.load("images/breads_menu.png"), (pygame.image.load("images/breads_menu.png").get_width() * 2, pygame.image.load("images/breads_menu.png").get_height() * 2))
